d972fd057e4d996798fa9d88491eca457c463b1c
Aaron Yao <57114330+ayao1998@users.noreply.github.com>
2021-02-13 19:12:25 -0800

    added secondary activity

--- app/src/main/java/com/example/zothikes/DisplayUserInfo.kt
+++ app/src/main/java/com/example/zothikes/DisplayUserInfo.kt
@@ -1,17 +1,34 @@
 package com.example.zothikes
 
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * to do:
+ * button should start new map activity with the hiking trails
+ * if possible make it so user doesn't have to retype everything after hitting back button
+ */
 
 class DisplayUserInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_user_info)
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
-        val textView = findViewById<TextView>(R.id.testMessage).apply {
-            text = message
+
+        //confirm button code
+        findViewById<Button>(R.id.confirm_button).setOnClickListener{
+            Toast.makeText(this, "Information confirmed.", Toast.LENGTH_SHORT).show()
         }
+
+        //set TextViews to user data from previous activity
+        findViewById<TextView>(R.id.confirm_name).apply {text = intent.getStringExtra("name")}
+        findViewById<TextView>(R.id.confirm_age).apply {text = intent.getStringExtra("age")}
+        findViewById<TextView>(R.id.confirm_height).apply {text = intent.getStringExtra("height")}
+        findViewById<TextView>(R.id.confirm_weight).apply {text = intent.getStringExtra("weight")}
+        findViewById<TextView>(R.id.confirm_gender).apply {text = intent.getStringExtra("gender")}
+        findViewById<TextView>(R.id.confirm_goal).apply {text = intent.getStringExtra("goal")}
+
     }
 }
--- app/src/main/java/com/example/zothikes/MainActivity.kt
+++ app/src/main/java/com/example/zothikes/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.zothikes
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * to do:
+ * create an app icon
+ * make toast darker to cover button
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,23 +21,17 @@ class MainActivity : AppCompatActivity() {
 
         //List containing user input. Goes in order of name, age, height, weight, gender, target weight.
         val user_data = mutableListOf<String>()
+
         //Finish button action
         findViewById<Button>(R.id.finish_button).setOnClickListener{
             submitInfo(it, user_data)
-
         }
 
     }
 
-    /**
-     * to do:
-     * verify data is correctly extracted from user inputs
-     * fix keyboard not being hidden
-     * create an app icon
-     * make toast darker to cover button
-     */
-
+    //adds data to muteablelist and passes data to next activity on click of finish button
     private fun submitInfo(view: View, user_data: MutableList<String>) {
+        user_data.clear() // prevent duplicates if user is making edits.
         user_data += findViewById<EditText>(R.id.name_input).text.toString()
         user_data += findViewById<EditText>(R.id.age_input).text.toString()
         user_data += findViewById<EditText>(R.id.height_input).text.toString()
@@ -41,10 +40,15 @@ class MainActivity : AppCompatActivity() {
         user_data += findViewById<EditText>(R.id.goal_input).text.toString()
         Toast.makeText(this, "Information updated.", Toast.LENGTH_SHORT).show()
 
-        val editText = findViewById<EditText>(R.id.goal_input)
-        val message = editText.text.toString()
+        //pass data to next activity screen
         val intent = Intent(this, DisplayUserInfo::class.java).apply{
-            putExtra(EXTRA_MESSAGE, message)
+            //not sure how to pass the entire user_data MuteableList, so doing each String individually for now
+            putExtra("name", user_data[0])
+            putExtra("age", user_data[1])
+            putExtra("height", user_data[2])
+            putExtra("weight", user_data[3])
+            putExtra("gender", user_data[4])
+            putExtra("goal", user_data[5])
         }
         startActivity(intent)
     }
